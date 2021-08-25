92bbb5bcfd6bce7fb72f004af0200bbc5988642f
Daniel Rozenberg <rodaniel@amp.dev>
2021-08-25 17:08:06 +0100

    Fix incorrect cookie name for opt-in

--- src/rtv.ts
+++ src/rtv.ts
@@ -30,7 +30,7 @@ export async function chooseRtv(request: ServerRequest): Promise<string> {
   // Choose which channel to opt in to based on the following order:
   const channelChooser = [
     // Opt-in cookie value:
-    Cookie.parse(request.headers.get('cookie') ?? '').__Host_AMP_OPT_IN,
+    Cookie.parse(request.headers.get('cookie') ?? '')['__Host-AMP_OPT_IN'],
     // Query param (?optin=channel-name):
     request.query.get('optin'),
     // LTS request:
