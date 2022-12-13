edcd77353cf709f2b7da1a701254dd4699c88306
Daniel Rozenberg <rodaniel@google.com>
2022-12-13 16:03:53 -0500

    Updates to match changes in Cloudflare/Wrangler infrastructure

--- esbuild.js
+++ esbuild.js
@@ -5,7 +5,7 @@ const textReplacePlugin = require('esbuild-plugin-text-replace');
 esbuild
   .build({
     bundle: true,
-    outfile: 'dist/worker.js',
+    outfile: 'dist/worker.mjs',
     entryPoints: ['src/index.ts'],
     plugins: [
       ignorePlugin([
@@ -14,7 +14,7 @@ esbuild
         },
       ]),
       textReplacePlugin({
-        include: /brotli_wasm(?:_bg?)\.js$/,
+        include: /brotli_wasm(?:_bg)?\.js$/,
         pattern: [
           ["import * as wasm from './brotli_wasm_bg.wasm';", ''],
           ['wasm.', 'globalThis.__BROTLI_WASM_INSTANCE.'],
@@ -22,7 +22,7 @@ esbuild
       }),
     ],
     sourcemap: true,
-    format: 'cjs',
+    format: 'esm',
     logLevel: 'info',
   })
   .catch(() => process.exit(1));
